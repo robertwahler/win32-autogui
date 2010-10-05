9b4b6f08db2f42a9d69c7a76ae8542daafe0f3b9
Robert Wahler <robert@gearheadforhire.com>
2010-10-05 14:59:32 -0400

    add VERSION to .yardopts

--- lib/basic_gem.rb
+++ lib/basic_gem.rb
@@ -5,9 +5,10 @@
 module BasicGem
   
   # Contents of the VERSION file
-  # VERSION example format: 0.0.1
   #
-  # @return [String] version the contents of the version file in #.#.# format
+  # Example format: 0.0.1
+  #
+  # @return [String] the contents of the version file in #.#.# format
   def self.version
     version_info_file = File.join(File.dirname(__FILE__), *%w[.. VERSION])
     File.open(version_info_file, "r") do |f|
