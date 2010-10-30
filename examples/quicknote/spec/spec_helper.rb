53c1787dae01d7a25beb537359d97258dbd43d57
Robert Wahler <robert@gearheadforhire.com>
2010-10-30 14:19:56 -0400

    spec_helper.rb fullpath returns windows formatted path on cygwin

--- examples/quicknote/spec/spec_helper.rb
+++ examples/quicknote/spec/spec_helper.rb
@@ -17,7 +17,9 @@ require 'aruba/api'
 
 # aruba helper, returns full path to files in the aruba tmp folder
 def fullpath(filename)
-  File.expand_path(File.join(current_dir, filename))
+  path = File.expand_path(File.join(current_dir, filename))
+  path = `cygpath -w #{path}`.chomp if path.match(/^\/cygdrive/)  # cygwin?
+  path
 end
 
 Spec::Runner.configure do |config|
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,9 @@ require File.expand_path(File.dirname(__FILE__) + '/applications/calculator')
 
 # aruba helper, returns full path to files in the aruba tmp folder
 def fullpath(filename)
-  File.expand_path(File.join(current_dir, filename))
+  path = File.expand_path(File.join(current_dir, filename))
+  path = `cygpath -w #{path}`.chomp if path.match(/^\/cygdrive/)  # cygwin?
+  path
 end
 
 Spec::Runner.configure do |config|
