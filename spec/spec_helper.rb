3e0c7faa4e320d92805c284e880994b99f47233a
Robert Wahler <robert@gearheadforhire.com>
2010-11-21 14:27:12 -0500

    spec aruba helper fullpath, get_file_contents

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,9 +9,25 @@ require 'spec'
 require 'spec/autorun'
 require 'aruba/api'
 
-# aruba helper, returns full path to files in the aruba tmp folder
+# aruba helpers
+#
+# @return full path to files in the aruba tmp folder
 def fullpath(filename)
-  File.expand_path(File.join(current_dir, filename))
+  path = File.expand_path(File.join(current_dir, filename))
+  if path.match(/^\/cygdrive/)
+    # match /cygdrive/c/path/to and return c:\\path\\to
+    path = `cygpath -w #{path}`.chomp
+  elsif path.match(/.\:/)
+    # match c:/path/to and return c:\\path\\to
+    path = path.gsub(/\//, '\\')
+  end
+  path
+end
+# @return the contents of "filename" in the aruba tmp folder
+def get_file_contents(filename)
+  in_current_dir do
+    IO.read(filename)
+  end
 end
 
 Spec::Runner.configure do |config|
