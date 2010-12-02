315fd25bbd4d4b6e85d09afab5b940c9d40e29c5
Robert Wahler <robert@gearheadforhire.com>
2010-12-02 15:15:05 -0500

    move aruba helpers to spec/aruba_helper.rb
    This allows use by both cucumber and rspec

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
 require 'rubygems'
 require 'basic_gem'
-require 'aruba'
 require 'spec/expectations'
+require 'aruba'
+require File.expand_path(File.dirname(__FILE__) + '/../../spec/aruba_helper')
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,27 +8,7 @@ require 'basic_gem'
 require 'spec'
 require 'spec/autorun'
 require 'aruba/api'
-
-# aruba helpers
-#
-# @return full path to files in the aruba tmp folder
-def fullpath(filename)
-  path = File.expand_path(File.join(current_dir, filename))
-  if path.match(/^\/cygdrive/)
-    # match /cygdrive/c/path/to and return c:\\path\\to
-    path = `cygpath -w #{path}`.chomp
-  elsif path.match(/.\:/)
-    # match c:/path/to and return c:\\path\\to
-    path = path.gsub(/\//, '\\')
-  end
-  path
-end
-# @return the contents of "filename" in the aruba tmp folder
-def get_file_contents(filename)
-  in_current_dir do
-    IO.read(filename)
-  end
-end
+require 'aruba_helper'
 
 Spec::Runner.configure do |config|
    config.include Aruba::Api
