223c66a405c316273bddd5774e87523575bcc8b9
Robert Wahler <robert@gearheadforhire.com>
2011-08-17 10:53:51 -0400

    add Rspec 2+ filter features to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,5 +14,8 @@ require File.expand_path(File.dirname(__FILE__) + '/applications/calculator')
 require File.expand_path(File.dirname(__FILE__) + '/applications/notepad')
 
 RSpec.configure do |config|
-   config.include Aruba::Api
+  config.include Aruba::Api
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
